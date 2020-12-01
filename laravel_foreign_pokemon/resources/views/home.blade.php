6b8e917d79847a6f7fb4495128a8b005a6fbc063
Karis Papadopoulos <60817321+SEVERECLICK-dev@users.noreply.github.com>
2020-12-01 11:33:18 +0100

    ajout structure de base

---
+++ laravel_foreign_pokemon/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('template.template')
+
+@section('content')
+<h1 class="text-center mt-5">Pokemons</h1>
+<div class="container mt-5">
+    <h5>coucou</h5>
+</div>
+@stop
---
+++ laravel_foreign_pokemon/resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-dark p-3">
+    <ul class="container nav">
+        <li class="nav-item">
+            <a class="nav-link text-white active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="/pokemons">Pokemons</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="/modif-pokemons">Modifier Pokemon</a>
+        </li>
+    </ul>
+</div>
